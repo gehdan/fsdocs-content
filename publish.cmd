bf0a29492cb9dd936ced17f5093d6fb4b9408382
dko <dko@nv.local>
2019-08-16 16:39:47 +0200

    Publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -1,20 +1,33 @@
 set versiondir=v4.0
 set currentpath=%cd%
 
-REM prüfen, ob das ziel-Repo clean ist.
+REM prüfen, ob das aktuelle Repo clean ist und auf neusten Stand bringen
+cd %currentpath%
+set gitout=
+for /f "tokens=*" %%i in ('git status --porcelain') do set gitout=%%i
+IF NOT "%gitout%" == "" (
+    set errortext=Der Workspace im content-Repository ist nicht clean
+    goto displayerror
+)
+git clean -fdx
+git pull
+
+REM prüfen, ob das ziel-Repo clean ist und auf neusten Stand bringen
 cd ..\fsdocs
 set gitout=
 for /f "tokens=*" %%i in ('git status --porcelain') do set gitout=%%i
 IF NOT "%gitout%" == "" (
-    set errortext=workspace is not clean
+    set errortext=Der Workspace im fsdocs-Repository ist nicht clean
     goto displayerror
 )
+git clean -fdx
 git pull
 
 REM die dokumentation rendern
 cd %currentpath%
 docfx\docfx.exe docfx.json
 
+REM die dokumentation in das ziel-Repo kopieren
 cd ..\fsdocs
 rd %versiondir% /s /q
 md %versiondir%
